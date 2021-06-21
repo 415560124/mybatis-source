5415639ee32459f3bc0bab1117cb5f95af153df3
Rhy <415560124@qq.com>
2021-06-21 14:54:38 +0800

    1.添加maven源码插件

--- mybatis-demo/src/test/java/com/rhy/mybatisdemo/MybatisDemoApplicationTests.java
+++ mybatis-demo/src/test/java/com/rhy/mybatisdemo/MybatisDemoApplicationTests.java
@@ -8,7 +8,7 @@ class MybatisDemoApplicationTests {
 
     @Test
     void contextLoads() {
-        System.out.println(333);
+        System.out.println(444);
         System.out.println(888);
     }
 
